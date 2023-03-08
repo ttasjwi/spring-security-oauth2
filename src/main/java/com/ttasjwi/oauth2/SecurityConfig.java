08cac73a11df1e8367b1b9a3f3f03b60c9a1498f
ttasjwi <ttasjwi920@gmail.com>
2023-03-08 16:57:27 +0900

    예전 방식의 보안 설정 방식 : WebSecurityConfigurerAdapter

---
+++ src/main/java/com/ttasjwi/oauth2/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.ttasjwi.oauth2;
+
+public class SecurityConfig // extends WebSecurityConfigurerAdapter
+{
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // ...
+//    }
+}
