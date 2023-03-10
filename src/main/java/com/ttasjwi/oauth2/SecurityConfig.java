539140983a1cd76eece6e55695009592bdba67c5
ttasjwi <ttasjwi920@gmail.com>
2023-03-10 15:20:35 +0900

    여러개의 DefaultAuthenticationEntryPoint -> 상황에 맞는 것 사용

--- src/main/java/com/ttasjwi/oauth2/SecurityConfig.java
+++ src/main/java/com/ttasjwi/oauth2/SecurityConfig.java
@@ -11,7 +11,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().anyRequest().authenticated();
-        http.formLogin(); // Form 인증 -> LoginUrlAuthenticationEntryPoint 등록
+
+        // 여러개의 DefaultAuthenticationEntryPoint -> 상황에 맞는 것 사용
+        http.httpBasic();
+        http.formLogin();
         return http.build();
     }
 }
