05b33487e677bda2f4eb9fa32f6f35c8260ea1a1
ttasjwi <ttasjwi920@gmail.com>
2024-06-06 18:19:24 +0900

    OAuth 2.0 Client 자동설정에 의한 초기화 과정 이해

--- app/src/main/kotlin/com/ttasjwi/oauth2/IndexController.kt
+++ app/src/main/kotlin/com/ttasjwi/oauth2/IndexController.kt
@@ -1,29 +1,13 @@
 package com.ttasjwi.oauth2
 
-import com.ttasjwi.oauth2.support.logging.getLogger
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class IndexController(
-    private val clientRegistrationRepository: ClientRegistrationRepository
-) {
-
-    companion object {
-        private val log = getLogger(IndexController::class.java)
-    }
+class IndexController {
 
     @GetMapping("/")
     fun index(): String {
-        val clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak")
-
-        val clientId = clientRegistration.clientId
-        log.info { "clientId = $clientId" }
-
-        val redirectUri = clientRegistration.redirectUri
-        log.info { "redirectUri = $redirectUri" }
-
         return "index"
     }
 }
