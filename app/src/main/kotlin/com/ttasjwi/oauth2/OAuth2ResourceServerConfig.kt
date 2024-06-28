68effbcd557a252c7d22811feef36589d05633d5
ttasjwi <ttasjwi920@gmail.com>
2024-06-28 15:36:26 +0900

    JwtDecoder 생성 방법

--- app/src/main/kotlin/com/ttasjwi/oauth2/OAuth2ResourceServerConfig.kt
+++ app/src/main/kotlin/com/ttasjwi/oauth2/OAuth2ResourceServerConfig.kt
@@ -1,13 +1,19 @@
 package com.ttasjwi.oauth2
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class OAuth2ResourceServerConfig {
+class OAuth2ResourceServerConfig(
+    private val properties: OAuth2ResourceServerProperties
+) {
 
     @Bean
     fun jwtSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -19,5 +25,19 @@ class OAuth2ResourceServerConfig {
                 it.jwt(Customizer.withDefaults())
             }
         return http.build()
+    }
+
+//    @Bean
+//    fun JwtDecoder(): JwtDecoder {
+//        return JwtDecoders.fromIssuerLocation(properties.jwt.issuerUri)
+////        return JwtDecoders.fromOidcIssuerLocation(properties.jwt.issuerUri)
+//    }
+
+    @Bean
+    fun JwtDecoder(): JwtDecoder {
+        return NimbusJwtDecoder
+            .withJwkSetUri(properties.jwt.jwkSetUri)
+            // 기본 알고리즘 : RS256
+            .jwsAlgorithm(SignatureAlgorithm.RS256).build()
     }
 }
